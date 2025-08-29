dd8813cadfde40e3da61dc6e2e31910f697172be
Eduardo <135570112+Dudusegovia@users.noreply.github.com>
2025-08-28 22:24:18 -0300

    Update fuzzy_means.py

--- fuzzy_means.py
+++ fuzzy_means.py
@@ -77,11 +77,11 @@ def K_FUZZY(dataf, num_clusters, rodadas_max=100):
 
 
 # data deve conter seu dataframe de dados já tratados
-data = pd.read_excel("NAZEMI.xlsx")
+data = pd.read_excel("dados.xlsx")
 
 
 if __name__ == "__main__":
-    K_FUZZY(dataf=data, num_clusters=10, rodadas_max=100).to_excel("resultado.xlsx", index=False) # A função retorna um dataframe atualizado com a coluna 'cluster' adicionada
+    K_FUZZY(dataf=data, num_clusters=3, rodadas_max=100).to_excel("resultado.xlsx", index=False) # A função retorna um dataframe atualizado com a coluna 'cluster' adicionada
 
 
 
